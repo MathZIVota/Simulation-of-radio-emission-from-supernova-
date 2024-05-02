0a469b3c9cfda51c1a20e875b453b3559cbc0ee2
MathZIVota <106335470+MathZIVota@users.noreply.github.com>
2024-05-02 14:29:50 +0300

    Add modified version program
    Graphs for the rarefaction area. Added a graph for T

--- cases/riemann/Program/main.cpp
+++ cases/riemann/Program/main.cpp
@@ -68,23 +68,8 @@ public:
         std::cout << "Left: " << left_state.pressure << " " << left_state.density  << " " << left_state.velocity << "\n";
         std::cout << "Right: " << right_state.pressure << " " << right_state.density << " " << right_state.velocity << "\n";
     }
-
 };
 
-class Solver{
-public:
-    Tube tube;
-
-    Solver(Tube t){
-        tube = t;
-    }
-    
-
-
-    double sound_speed(){
-        return sqrt(tube.gamma * tube.left_state.pressure/tube.left_state.density);
-    }
-};
 // sound speed: 1.1832159566199232
 // double a0 = 1.1832159566199232;
 
@@ -97,12 +82,14 @@ public:
         b[1] = rho_u;
         b[2] = rho_E;
     }
+
     Сond& operator=(const Сond& data) {
         if(&data != this){
             for(int i=0; i<3; i++) b[i] = data.b[i];
         }
         return *this;
     }
+
     Сond operator+(const Сond& data) const{
         //Сond tmp;
         //tmp.Set();
@@ -134,69 +121,158 @@ void Func1(vector<Сond> &A){
     }   
 }
 
+class TUBE{
+    private:
+    void Set_vector_Value(vector<vector<double>> &a,double a4, double a1, int j){
+        vector<double> tmp(Nx);
+        a.push_back(tmp);
+        for(size_t i=0; i<Nx; i++){
+            a[j][i] = a4;
+            if(x[i] > 0) a[j][i] = a1;
+        }
+    }
+    
+    class Geometry
+    {
+    public:
+        double xl, xr, xi;
+        double x1, x2, x3, x4; 
+        Geometry(double in_xl=-1, double in_xr=1, double in_xi=0){
+            xl = in_xl;
+            xr = in_xr;
+            xi = in_xi;
+            x1 = xi;
+            x2 = xi;
+            x3 = xi;
+            x4 = xi;
+        }
+    };
+
+    public:
+    int Nx = 0;
+    int Nt = 0;
+    vector<double> x, t;
+    vector<vector<double>> p,u,rho,T,e,E,h,S;
+    Geometry geom;
+    void Set_Geometry(int N1 = 501, int N2 = 300, double xl = -1, double xr = 1){
+        Nx = N1;
+        Nt = N2;
+        geom.xi = 0;
+        geom.xl = xl;
+        geom.xr = xr;
+
+        double dx = (xr-xl)/(Nx-1);
+        for (double i=0; i<Nx; i++) {
+            double xi = xl+i*dx;
+            x.push_back(xi);
+        }
+
+        double dt = 1./(Nt);
+        for (int i=0; i<Nt; i++){
+            t.push_back((double)i*dt);
+        }
+    };
+    
+    void Set_Initial_Values(vector<double> initial_value = {1,1,0,0.1,0.125,0}){
+        Set_vector_Value(p, initial_value[0], initial_value[3], 0);
+        Set_vector_Value(rho, initial_value[1], initial_value[4], 0);
+        Set_vector_Value(u, initial_value[2], initial_value[5], 0);
+    };
+
+    void Print(){
+        for(int i=0; i<Nx; i++){
+            //printf("", x[i]);
+            if(i % 50 == 0) printf("%lf:%lf %lf %lf", p[0][i], rho[0][i], u[0][i]);
+            cout << endl;
+        }
+    };
+
+};
+
+
+class SOLVER{
+public:
+    int Nt;
+    vector<double> t;
+    TUBE tube;
+    // i1 - передний фронт волны разрежения, i2 - задний, i3 - контактный разрыв, i4 - ударная волна
+    int i1,i2,i3,i4; 
+
+    SOLVER(){
+        Nt = 200;
+        double dt = 1./Nt;
+        for (int i=0; i<Nt; i++){
+            t.push_back((double)i*dt);
+        }
+        tube.Set_Geometry();
+        tube.Set_Initial_Values();
+        i1 = tube.geom.xi;
+        i2 = tube.geom.xi;
+        i3 = tube.geom.xi;
+        i4 = tube.geom.xi;
+    }
+};
+
 int main(){
-    Tube tube({-1,1,0},{1,1,0},{0.125, 0.1, 0});
-    tube.print();
-    Solver solver(tube);
-    int Nx = 2000;
-    double L = 1, t_max = 1, c = 1;
+    int Nx = 301;
+    int Nt = 350;
+
+    double t_max = 1;
     double x_l = -1, x_m = 0, x_r = 1;
     double dx = (x_r-x_l)/(Nx-1);
-    int Nt = 350;
+    
     double dt = t_max/Nt;
-    
-    //double dt = T/Nt;
-    //int Nt = round(T/dt) + 1;
-    
+
     vector<double> t,x;
     cout << "t("<< t_max << " "<< Nt << " " << dt <<"):";
 
     // Разбиение времени
     for (int i=0; i<Nt; i++){
         t.push_back((double)i*dt);
-        cout << i*dt << " ";
+    //    cout << i*dt << " ";
     }
     cout << endl;
 
-    //Разбиение координаты
-    cout << "x("<< L << " "<< Nx << " " << dx <<"):";
+    // Разбиение координаты
+    cout << "x("<< 2 << " "<< Nx << " " << dx <<"):";
     for (double i=0; i<Nx; i++) {
         double xi = x_l+i*dx;
         x.push_back(xi);
-        cout << xi << " ";
+    //    cout << xi << " ";
     }
     cout << endl << endl;
     
-
+    // Основные величины
     vector<vector<double>> p,u,rho,T,e,E,h,S;
-    double p4 = 1, p1 = 0.1, u4 = 0, u1 = 0, rho4 = 1, rho1 = 0.1, T1 = 1.4, T4 = 1;
-    double gamma = 1.4;
-    
-    //cout << "sound_speed:" <<  a0 << endl;
-    
-    cout << "---" << endl;
-    double D = 0, M = 0, u_2 = 0, p_2 = 0, rho_2 = 0, u_3=0, p_3=0, rho_3=0;
-    double c1 = sqrt(gamma*p1/rho1);
-    cout << "x size:"<< Nx << " "<< endl;
+    double p4 = 1, p1 = 0.125, u4 = 0, u1 = 0, rho4 = 1, rho1 = 0.1;
+    double R = 8.31, T1 = p1/rho1, T4 = p4/rho4;
+    double gamma = 1.4;    
     double eps = 1e-15;
-    D = rho4/rho1*(p1-p4)/(rho1-rho4);
-    // cout << D << " - " << c1  << " /" << x[Nx/2] << endl;
     
+    // Расчеты    
+    cout << " -----------Start_Calculation-------------- " << endl;
     int i0 = 0, i1=Nx/2, i2=Nx/2, i3=Nx/2, i4=Nx/2, i5 = Nx-1, i_initial = 200;
+    double M = 0, u_2 = 0, p_2 = 0, rho_2 = 0, u_3=0, p_3=0, rho_3=0;
+    double T_2 = 0, T_3 = 0;
+    double D = rho4/rho1*(p1-p4)/(rho1-rho4); //скорость ударной волны
+    double c1 = sqrt(gamma*p1/rho1), Uc = 0, c3 = 0; // скорость звука справа
+    double c4 = sqrt(gamma*p4/rho4), a = 0; // скорость звука слева
     p_3 = p4;
     rho_3 = rho4;
-    //cout << t[200] << endl;
-    cout << " -----------Begin_Calculation-------------- " << endl;
+    double A4 = 1;
+    
+    vector<double> emp(Nx,{0});
     for (size_t k = 0; k < Nt; k++)
     {
-        p.push_back(x);
-        u.push_back(x);
-        rho.push_back(x);
+
+        p.push_back(emp);
+        u.push_back(emp);
+        rho.push_back(emp);
         //e.push_back(x);
         //E.push_back(x);
         //h.push_back(x);
-        T.push_back(x);
-        S.push_back(x);
+        T.push_back(emp);
+        S.push_back(emp);
         //M.push_back(x);
         for(size_t i = 0; i < Nx; i++){
             p[k][i] = p4;
@@ -209,69 +285,111 @@ int main(){
                 rho[k][i] = rho1;
                 T[k][i] = T1;
             } 
-            M = 1.6;
-
         }
-
-        if(k > 0){
-            //cout << "! " << k << "/ " << p_3 << ", " << p1 << "/ " << rho_3 << ", " << rho1 << "/" << endl;//<< p[0][199] << "," << p[0][200] << "," << p[0][201]  << endl;
-            if(k > 1) D = rho_3*u_3/(rho_3-rho1);//rho_3/rho1*(p_3-p1)/(rho_3-rho1); ////////
-            else D = rho4/rho1*(p1-p4)/(rho1-rho4);//M*c1;
+        if(k==0){    
+            M = 1.6;
+            D = M*c1;
+            u_3 = u1;
+            p_3 = p1;
+            rho_3 = rho1;
+            //D = rho4/rho1*(p1-p4)/(rho1-rho4);
+            Uc = 2*c1*(M-1/M)/(gamma+1);
+        }
+        else if(k > 0){
+            c3 = (gamma+1)*Uc/2-c4;
+            cout << "! " << k  << ":" << Uc << " " << D << endl; 
+            //<< "/ " << p_3 << ", " << p1 << "/ " << rho_3 << ", " << rho1 << "/" << endl;//<< p[0][199] << "," << p[0][200] << "," << p[0][201]  << endl;
+            //if(k < 1) D = (p_3-p1)/(rho1*u_3);//rho_3/rho1*(p_3-p1)/(rho_3-rho1); ////////
+            //else D = rho4/rho1*(p1-p4)/(rho1-rho4);//M*c1;
             
-            cout << "! " << D << " " << D*dt << endl;
-            cout << "! " << c1 << " " << c1*dt << endl;
-
-            int j = i3;
-            while(x[j] < x[i3]+c1*dt) j++;
+            //D = rho_3/rho1*(p1-p_3)/(rho1-rho_3);
+            //cout << "! " << D << " " << D*dt << endl;
+            //cout << "! " << c1 << " " << c1*dt << endl;
+            int j = i0;
+            while(x[j] < -c4*t[k]) j++;
+            i1 = j;
+            j = i2;
+            while(x[j] < c3*t[k]) j++;
+            i2 = j;
+            cout << "i:" << i3;
+            j = i3;
+            while(x[j] < Uc*t[k]) j++;
             i3 = j;
+            cout << " " << i3 << "|" << i4 << " ";
             j = i4;
-            while(x[j] < x[i4]+D*dt) j++;            
+            while(x[j] < D*t[k]) j++;            
             i4 = j;
-            
+            cout << i4 << endl;
 
-            if(i4 > Nx-1) i4 = Nx-1;
-            if(i3 > Nx-1) i3 = Nx-1;
+            if(i4 >= Nx) i4 = Nx-1;
+            if(i3 >= Nx) i3 = Nx-1;
+            if(i2 >= Nx) i2 = Nx-1;
+            if(i1 < 0) i1 = 0;
 
-            //p_3 = p1*pow(1-(gamma-1)*u_3/(2*c1),2*gamma/(gamma-1));
             p_3 = p1*(2*gamma*M*M-(gamma-1))/(gamma+1);
             rho_3 = rho1*(gamma+1)*M*M/((gamma-1)*M*M+2);
-            //rho_3 = rho1*((gamma-1)*p4+(gamma+1)*p_3)/((gamma-1)*p_3+(gamma+1)*p4);
-           // u_3 = c1*2*(M-1/M)/(gamma+1);
-             u_3 = sqrt(2*pow((p_3-p1),2)/(rho1*((gamma-1)*p1+(gamma+1)*p_3)));
-            //u_3 = 2*c1*(M-1/M)/(gamma+1);
-            //u_3 = 1 - 2*c1/(gamma-1)*pow(p_3/p4, (gamma-1)/2/gamma);
-            //u_3 = sqrt(2/rho1)*(p_3-p1)/sqrt((gamma-1)*p1+(gamma+1)*p_3);
+            u_3 = c1*2*(M-1/M)/(gamma+1);
+            T_3 = p_3/rho_3;
 
             p_2 = p_3;
             u_2 = u_3;
             rho_2 = rho4*pow(p_3/p4,1/gamma);
+            T_2 = p_2/rho_2;
 
-            cout << "|" << i2 << " " << i3 << " " << i4  << " " << u_3<< endl;
-            cout << x[i2] << " " << x[i3] << endl;
-            for(int i=i2; i<i3;i++){
-                rho[k][i] = rho_2;
-                p[k][i] = p_2;
-                u[k][i] = u_2;
-            }
-            cout << x[i3] << " " << x[i4] << endl;
-            for(int i=i3; i<i4 ; i++){
-                rho[k][i] = rho_3;
-                p[k][i] = p_3;
-                u[k][i] = u_3;
-            }
             
-            M = D/c1;
+            //rho_3 = rho1*((gamma-1)*p4+(gamma+1)*p_3)/((gamma-1)*p_3+(gamma+1)*p4);
+            //p_3 = p4*pow(1-(gamma-1)*u_3/(2*c1), 2*gamma/(gamma-1));
+            //u_3 = sqrt(2*pow((p_3-p1),2)/(rho1*((gamma-1)*p1+(gamma+1)*p_3)));
+            //p_3 = p1*pow(1-(gamma-1)*u_3/(2*c1),2*gamma/(gamma-1));
+            //T_3 = T1*(2*gamma*M*M-(gamma-1))/(gamma+1)*(2/((gamma+1)*M*M)+(gamma-1)/(gamma+1));
+            //u_3 = 2*c1*(M-1/M)/(gamma+1);
+            //u_3 = 1 - 2*c1/(gamma-1)*pow(p_3/p4, (gamma-1)/2/gamma);
+            //u_3 = sqrt(2/rho1)*(p_3-p1)/sqrt((gamma-1)*p1+(gamma+1)*p_3);
+
+            
+            //cout << "index:" << i2 << " " << i3 << " " << i4 << endl;
+            //cout << "rho:" << rho_2 << " " << rho_3 << " " << rho4 << endl;
+            
+
+            for(int i=i1; i<Nx;i++){
+                a = (-(gamma-1)*x[i]/t[k]+2*c4)/(gamma+1);
+
+                if(i<i2){
+                    rho[k][i] = 0;
+                    p[k][i] = pow(pow(a*a/gamma,gamma)/A4,1/(gamma-1));
+                    u[k][i] = (2*x[i]/t[k]+2*c4)/(gamma+1);
+                    T[k][i] = 0;
+                }
+                if(i>=i2 && i < i3){
+                    //if(k%50==0) cout << "i2-i3\n";
+                    rho[k][i] = rho_2;
+                    p[k][i] = p_2;
+                    u[k][i] = u_2;
+                    T[k][i] = T_2;
+                }
+                if(i >= i3 && i < i4){
+                    //if(k%50==0) cout << "i3-i4\n";
+                    rho[k][i] = rho_3;
+                    p[k][i] = p_3;
+                    u[k][i] = u_3;
+                    T[k][i] = T_3;
+                }
+            }
         }
         for(int i=0; i<Nx; i++){
             S[k][i] = log(p[k][i]/pow(rho[k][i], gamma));//+S[0][i];
         }
-        cout << "M = " << M  << " " << p_3/p4 << endl;
+        M = D/c1;
+        
+        //D = rho_3/rho1*(p_3-p1)/(rho_3-rho1);
+        //cout << "M = " << M  << " " << p_3/p4 << endl;
     }
     //cout << dx << " " << Nx << " | " << dt << " " << Nt << endl;
-    cout << " -----------End_Calculation-------------- " << endl;
+    cout << " -----------Finish_Calculation-------------- " << endl;
 
     //**********************************************************//
-     //*********************************************************//
+    //**********Вывод в файл**********************************//
+
     ofstream out1,out2;
     out2.open("x_t_diagram.txt");
     if(out2.is_open()){
@@ -293,7 +411,7 @@ int main(){
         }
     }
     out1.close();
-    cout << solver.sound_speed() << endl;
+    //cout << solver.sound_speed() << endl;
 
     
     out1.open("result_p.txt");
@@ -323,7 +441,7 @@ int main(){
         }
     }
     out1.close();
-     out1.open("result_S.txt");
+    out1.open("result_S.txt");
     if(out1.is_open()){
         for (int i=0; i<Nx; i++){
             out1 << x[i] << " ";
@@ -337,6 +455,20 @@ int main(){
     }
     out1.close();
 
+    out1.close();
+    out1.open("result_T.txt");
+    if(out1.is_open()){
+        for (int i=0; i<Nx; i++){
+            out1 << x[i] << " ";
+        }
+        out1 << endl;
+        for (int i=0; i<Nt; i++){
+            out1 << t[i] << " ";
+            for(int j=0; j<Nx; j++) out1  << T[i][j] << " ";
+            out1 << endl;
+        }
+    }
+    out1.close();
 
    // cout << " -----------Begin_Plotting------------- " << endl;
    // system("plotting.exe");
--- cases/riemann/Program/plotting.py
+++ cases/riemann/Program/plotting.py
@@ -3,8 +3,8 @@ import numpy as np
 #import os
 #os.system('main.exe')
 def Func_Plot(n, axes, m):
-    str1 = ["P", "rho", "u", "S"]
-    str2 = ["result_p.txt", "result_rho.txt", "result_u.txt", "result_S.txt"]
+    str1 = ["P", "rho", "u", "S", "T"]
+    str2 = ["result_p.txt", "result_rho.txt", "result_u.txt", "result_S.txt", "result_T.txt"]
     for j in range(m):
         f = open(str2[j], 'r')
         try:
@@ -36,14 +36,19 @@ def Func_Plot(n, axes, m):
             #axes[i].set_title(, fontsize = 10, fontweight = 'heavy', color = 'red')
             axes[j, i].text(-1.2, 0.7, "t="+str(round(t[i*h],2))+" sec",fontsize = 8, fontweight = 'heavy', color = 'orange')
             axes[j, i].set_xlim(x[0]-0.2, x[len(x)-1]+0.2)
-            axes[j, i].set_ylim(-0.01, 1.1)
-            axes[j, i].plot([-1, -1],[1.2, 0], "black", linewidth = 3)
-            axes[j, i].plot([1, 1],[1.2, 0], "black", linewidth = 3)
+            if j==4:
+                axes[j, i].set_ylim(0.59, 1.41)
+            else:
+                axes[j, i].set_ylim(-0.01, 1.5)
+                
+            axes[j, i].plot([-1, -1],[2, 0], "black", linewidth = 3)
+            axes[j, i].plot([1, 1],[2, 0], "black", linewidth = 3)
             axes[j, i].grid()
             if(i == 0):
                 axes[j, i].set_ylabel(str1[j],fontsize=15, fontweight='bold')
         y = [0, 0.2, 0.4, 0.6, 0.8, 1]#[x/2 for x in range(0,3)]
-        plt.setp(axes, yticks = y)
+        plt.setp(axes[0:3], yticks = y)
+        plt.setp(axes[4], yticks = [0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8])
     
 
 #plt.plot([0, 1],[0, 1./0.621059], linewidth = 1)
@@ -53,8 +58,8 @@ def Func_Plot(n, axes, m):
 #plt.legend(["V = 0.621059",'a4 = 1.18322','a1 = 0.68313'])
 #plt.show()
 n_time = 2
-fig, axes = plt.subplots(5,n_time)
-Func_Plot(n_time, axes, 4)
+fig, axes = plt.subplots(6,n_time)
+Func_Plot(n_time, axes, 5)
 f = open("x_t_diagram.txt", 'r')
 try:
     data_plot = []
@@ -70,7 +75,7 @@ x_plot = np.array(data_plot[1])
 #print(speeds[0], x_plot)
 t_plot1 =  x_plot/speeds[0]
 t_plot2 =  x_plot/speeds[1]
-axes[4,0].plot(x_plot, t_plot1, x_plot, t_plot2)
-axes[4,0].legend(["Sound speed:"+str(speeds[0]), "Shock wave speed:"+str(speeds[1])])
+axes[5,0].plot(x_plot, t_plot1, x_plot, t_plot2)
+axes[5,0].legend(["Sound speed:"+str(speeds[0]), "Shock wave speed:"+str(speeds[1])])
 plt.show()
 
